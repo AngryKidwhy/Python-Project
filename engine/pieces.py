c5caddf0bb62a697628d004cae0e77dfd12c506d
AngryKidwhy <andvikpro@gmail.com>
2026-04-25 21:44:49 +0300

    feat Pawn

--- engine/pieces.py
+++ engine/pieces.py
@@ -58,3 +58,26 @@ class King(Piece):
     def get_valid_moves(self, board: 'Board') -> List[Move]:
         directions = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
         return self._get_sliding_moves(board, directions, 1)
+
+class Pawn(Piece):
+    def get_valid_moves(self, board: 'Board') -> List[Move]:
+        moves = []
+        dr = 1 if self.color == Color.White else -1
+
+        one_step = Position(self.pos.row + dr, self.pos.col)
+        if one_step.is_on_board() and board.get_piece_at(one_step) is None:
+            moves.append(Move(self.pos, one_step, self))
+
+            if not self.has_moved:
+                two_steps = Position(self.pos.row + 2 * dr, self.pos.col)
+                if board.get_piece_at(two_steps) is None:
+                    moves.append(Move(self.pos, two_steps, self))
+
+        for dc in [-1, 1]:
+            diag_pos = Position(self.pos.row + dr, self.pos.col + dc)
+            if diag_pos.is_on_board():
+                target_piece = board.get_piece_at(diag_pos)
+                if target_piece and target_piece.color != self.color:
+                    moves.append(Move(self.pos, diag_pos, self, target_piece))
+                    
+        return moves
